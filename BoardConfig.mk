5e76653d9f9c828575b52625b2d0b968a05b3ce7
Muhammed Nazim <mnm9994u@gmail.com>
2013-01-27 19:58:24 +0100

    Build with Linaro
    Change-Id: I3e275628e9d9930efe05e3806f3f58f7daf8107e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,6 +44,7 @@ BOARD_CUSTOM_BOOTIMG_MK := device/samsung/i9100g/shbootimg.mk
 # Inline kernel building
 TARGET_KERNEL_SOURCE := kernel/samsung/t1
 TARGET_KERNEL_CONFIG := cyanogenmod_i9100g_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro/bin/arm-linux-gnueabihf-
 
 # Init
 TARGET_PROVIDES_INIT := true
