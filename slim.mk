53972b9849cb2a847bd2f8954aae1fc5e577034e
Muhammed Nazim <mnm9994u@gmail.com>
2013-04-06 00:08:39 +0200

    Add some more tweaks
    Change-Id: I4c2eee61c7e78aaeb986865dda52ba71a8897283

--- slim.mk
+++ slim.mk
@@ -25,19 +25,19 @@ PRODUCT_MANUFACTURER := samsung
 
 #Extra Device info
 PRODUCT_PROPERTY_OVERRIDES += \
-	ro.device.cpu=Dual-core_1.2Ghz_Cortex-A9 \
-	ro.device.gpu=PowerVR-SGX540 \
-	ro.device.rear_cam=8MP \
-	ro.device.front_cam=2MP \
-	ro.device.screen_res=480x800
+    ro.device.cpu=Dual-core_1.2Ghz_Cortex-A9 \
+    ro.device.gpu=PowerVR-SGX540 \
+    ro.device.rear_cam=8MP \
+    ro.device.front_cam=2MP \
+    ro.device.screen_res=480x800
 
 #Boot Animation
 PRODUCT_COPY_FILES += \
-	vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/slim/prebuilt/hdpi/bootanimation.zip:system/media/bootanimation.zip
 
 #copy 00check
 PRODUCT_COPY_FILES += \
-	vendor/slim/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check
+    device/samsung/i9100g/69cpuinit:system/etc/init.d/69cpuinit
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=GT-I9100G TARGET_DEVICE=GT-I9100G BUILD_FINGERPRINT=samsung/GT-I9100G/GT-I9100G:4.1.2/JZO54K/I9100GXXLSR:user/release-keys PRIVATE_BUILD_DESC="GT-I9100G-user 4.1.2 JZO54K I9100GXXLSR release-keys"
